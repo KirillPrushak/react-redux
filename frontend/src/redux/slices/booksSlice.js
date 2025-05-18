2025164996ba81465b5e7009f62c4f5b543f5bd7
KirillPrushak <kirillprushak@gmail.com>
2025-05-18 14:30:45 +0300

    Moved isLoadingViaAPI to Redux

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -3,7 +3,10 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import createBookWithID from '../../utils/createBookWithID';
 import { setError } from './errorSlice';
 
-const initialState = [];
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+};
 
 export const fetchBook = createAsyncThunk(
   'books/fetchBook',
@@ -13,7 +16,10 @@ export const fetchBook = createAsyncThunk(
       return res.data;
     } catch (error) {
       thunkAPI.dispatch(setError(error.message));
-      throw error;
+      //Variant - 1 (предпочтительнее, отклоняем promise)
+      return thunkAPI.rejectWithValue(error);
+      //Variant - 2
+      //throw error
     }
   }
 );
@@ -23,39 +29,59 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload);
+      state.books.push(action.payload);
     },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload);
+      return {
+        ...action,
+        books: state.books.filter((book) => book.id !== action.payload),
+      };
     },
     toggleFavorite: (state, action) => {
-      state.forEach((book) => {
+      state.books.forEach((book) => {
         if (book.id === action.payload) {
           book.isFavorite = !book.isFavorite;
         }
       });
     },
   },
-  //OPTION 1
+
+  // OPTION 1 - Устаревший код
   // extraReducers: {
+  //   [fetchBook.pending]: (state) => {
+  //     state.isLoadingViaAPI = true;
+  //   },
   //   [fetchBook.fulfilled]: (state, action) => {
+  //     state.isLoadingViaAPI = false;
   //     if (action.payload.title && action.payload.author) {
-  //       state.push(createBookWithID(action.payload, 'API'));
+  //       state.books.push(createBookWithID(action.payload, 'API'));
   //     }
   //   },
+  //   [fetchBook.rejected]: (state) => {
+  //     state.isLoadingViaAPI = false;
+  //   },
   // },
+
   //OPTION 2
   extraReducers: (bulder) => {
+    bulder.addCase(fetchBook.pending, (state) => {
+      state.isLoadingViaAPI = true;
+    });
     bulder.addCase(fetchBook.fulfilled, (state, action) => {
+      state.isLoadingViaAPI = false;
       if (action.payload.title && action.payload.author) {
-        state.push(createBookWithID(action.payload, 'API'));
+        state.books.push(createBookWithID(action.payload, 'API'));
       }
+    });
+    bulder.addCase(fetchBook.rejected, (state) => {
+      state.isLoadingViaAPI = false;
     });
   },
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 
-export const selectBooks = (state) => state.books;
+export const selectBooks = (state) => state.books.books;
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI;
 
 export default booksSlice.reducer;
